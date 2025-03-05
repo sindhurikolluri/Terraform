f19946643e293f16695beef4ebe25733b75d282e
sindhurikolluri <sindhurikolluri2020@gmail.com>
2025-03-05 17:05:35 -0500

    terraform

---
+++ ec2.tf
@@ -0,0 +1,11 @@
+# resource "aws_instance" "this" {
+# } 
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
